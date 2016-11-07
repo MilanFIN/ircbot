ef842dc1a0ba2a3ae5363010eae33b095f07efd0
MilanFIN <milan.makipaa@gmail.com>
2016-11-07 14:14:42 +0200

    Topic change method working
    should be able to change beginning of the topic depending on an external
    cue

--- ircbot.py
+++ ircbot.py
@@ -11,19 +11,27 @@ bot for IRCnet channels
 #import psutil
 import socket
 import time
-#regex?
-import re
+
 
 
 class ircBot():
     def __init__(self, network, port, nick, channel):
 
+
+        #storing the parameters
         self.network = network
         self.port = port
         self.nick = nick
         self.channel = channel
 
+        #defining commands that are static, can be added like examples as
+        #command-answer. Commands are recognized as !command from irc
+        self.commandDict = {
+            "komento": "vastaus 1",
+            "komento2": "vastaus2"
+        }
 
+        #connecting here
         self.irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.irc.connect((network.encode(), port)) # normally port=6667
         self.data = self.irc.recv (4096)
@@ -41,24 +49,30 @@ class ircBot():
         #keepalive, must answer pong if ping is recieved.
         if self.data.find('PING'.encode()) != -1:
             self.irc.send(('PONG ' + str(self.data.split()[1]) + '\r\n').encode())
-        #check for commands
-        if self.data.find(("PRIVMSG " + self.channel + " :!komento").encode()) != -1:
-            self.send("vastaus")
-            self.consolePrint("!komento noticed, answered")
+        #check for commands that are just answers from commandDict
+        if self.data.find(("PRIVMSG " + self.channel + " :" + "!").encode()) != -1:
+            strData = self.data.decode()
+            dump, command = strData.split("PRIVMSG " + self.channel + " :" + "!", 1)
+            command = command.strip()
+            if command in self.commandDict:
+                self.send(self.commandDict[command])
+                self.consolePrint("command detected: !" + command)
+                self.consolePrint("answered: " + self.commandDict[command])
+        #topicchange (placeholder)
         if self.data.find(("PRIVMSG " + self.channel + " :!topic").encode()) != -1:
-            self.topic("")
+            self.topic()
 
 
     def send(self, message):
         #sends the parameter message to irc channel
-        self.irc.send(('PRIVMSG '+self.channel + ' ' + str(message) + '\r\n').encode())
+        self.irc.send(('PRIVMSG '+self.channel + ' :' + str(message) + '\r\n').encode())
 
 
-    def topic(self, status):
+    def topic(self):
         self.irc.send(('TOPIC '+self.channel + '\r\n').encode())
         self.data = self.irc.recv (4096)
         if self.data.find((" testibot " + self.channel + " :").encode()) != -1:
-            strData = str(self.data)
+            strData = self.data.decode()  #str(self.data)
             startString = self.nick + " " + self.channel + " :"
             endString = ":"
 
@@ -68,20 +82,21 @@ class ircBot():
 
             if topic.startswith("OFF"): #changing from OFF to ON
                 newTopic = "ON" + topic[3:]
-                self.setTopic(" "+ newTopic)
+                self.setTopic(newTopic)
             elif topic.startswith("ON"): #changing from ON to OFF
                 newTopic = "OFF" + topic[2:]
-                self.setTopic(" "+newTopic)
+                self.setTopic(newTopic)
             else:
                 self.consolePrint("topic is weird")
 
+
     def setTopic(self, topic = ""):
-        self.irc.send(('TOPIC '+self.channel + topic + '\r\n').encode())
+        self.irc.send(("TOPIC " + self.channel + " :" + topic + '\r\n').encode())
         if topic:
             self.consolePrint("Attempting to set new topic as: " + topic)
     def consolePrint(self, text):
         #prints stuff in a "clean" manner
-        output = "[" + time.strftime("%d-%m-%Y | %H:%M:%S") + "] " + text
+        output = "["+  time.strftime("%H:%M") + "] " + text
         print(output)
 
 
@@ -96,7 +111,7 @@ botti = ircBot("open.ircnet.net", 6667, "testibot", "#ircbottesti")
 def main():
     while True:
         botti.recieve()
-        time.sleep(0.1)
+        #time.sleep(0.1)
 
 
 
